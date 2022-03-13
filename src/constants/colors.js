e5ca05fd831c0b1421083256bc38cbaa852157d6
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-03-13 18:48:16 +0100

    chore: Add colors file

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  purple: '#7f7fff',
+  white: '#fff',
+  background: '#112D42',
+  orange: '#FF8137',
+  lightBackground: 'rgba(17, 45, 66, 0.62)',
+  transparentBackground: 'rgba(17, 45, 66, 0.68)',
+};
+
+export default COLORS;
