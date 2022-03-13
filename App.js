b4c7d1b6512f524e9239df5a039be88b10e4e476
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-03-13 17:06:45 +0100

    feat: Wrapped pp component in a provider

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Navigator from './src/navigation/navigation';
+import {Provider} from 'react-redux';
+import store from './src/store/store';
 
 const App = () => {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 };
 
 export default App;
